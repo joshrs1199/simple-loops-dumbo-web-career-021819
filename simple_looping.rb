7ee0fa64ed25f7457e0fe2bb4aa07d9f61641522
Joshua Rivera-Sampayo <josh.rs1199@gmail.com>
2019-02-11 07:03:07 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -22,7 +22,7 @@ def loop_iterator(number_of_times)
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  7.times do 
+  7.times do
     puts phrase
   end
 end
